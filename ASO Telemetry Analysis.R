011c79c31bd038454ac6b199ed997819e8f0af06
Jake Nichols <nichols.jacob@gmail.com>
2020-06-07 12:06:59 -0400

    add startlists and distance cleaner to telemetry

--- ASO Telemetry Analysis.R
+++ ASO Telemetry Analysis.R
@@ -1,10 +1,20 @@
 
 library(tidyverse)
 library(rjson)
+library(RMySQL)
+library(DBI)
+
+con <- dbConnect(MySQL(),
+                 host='localhost',
+                 dbname='cycling',
+                 user='jalnichols',
+                 password='braves')
 
 #
 
-d <- readr::read_rds('tdf-stage20-telemetry.rds')
+STAGE <- 19
+
+d <- readr::read_rds(paste0('Stage Telemetry/tdf-stage', STAGE, '-telemetry.rds'))
 
 #
 
@@ -46,17 +56,42 @@ for(x in 1:length(d)) {
 }
 
 #
+#
+#
+
+relevant_distances <- tibble(
+  
+  start = c(29, 86, 8, 19, 11, 51.5, 33, 12, 43, 23.5),
+  end = c(19, 74, 0, 0, 0, 38, 0, 0, 31, 17.5),
+  
+  climb = c("Lautaret to Galibier", "Izoard final 12km", "Planche Belles Filles",
+            "Tourmalet all", "Prat d'Albis", "Iseran", "Vall Thorens full",
+            "Val Thorens last 12km", "Ancizan all", "Final climb"),
+  
+  stage = c(18, 18, 6, 14, 15, 19, 20, 20, 12, 5)
+  
+)
 
 
+#
+#
 #
 
 all_data <- bind_rows(results_list) %>%
   
   mutate(actual_time = lubridate::parse_date_time(str_sub(time, 1, 19), orders = "%d/%m/%y HMS")) %>%
   
-  mutate(on_front = ifelse(distance_left > 43, "QSP",
-                           ifelse(distance_left > 8.2, "Jumbo",
-                           ifelse(distance_left > 6.6, "FDJ", "Pinot")))) %>%
+  arrange(bib, actual_time) %>%
+  
+  mutate(dl_025 = ifelse(distance_left < 0.25, actual_time, NA)) %>%
+  
+  group_by(bib) %>%
+  filter(actual_time <= min(dl_025, na.rm = T)) %>%
+  ungroup() %>%
+  
+  #mutate(on_front = ifelse(distance_left > 43, "QSP",
+  #                         ifelse(distance_left > 8.2, "Jumbo",
+  #                         ifelse(distance_left > 6.6, "FDJ", "Pinot")))) %>%
   mutate(yellow_secs = ifelse(yellow == TRUE, behind_leader, NA)) %>%
   
   group_by(block) %>%
@@ -71,13 +106,80 @@ all_data <- bind_rows(results_list) %>%
   
   group_by(block, t = floor(bib / 10)) %>%
   mutate(tm_in_yellow = sum(in_yellow_grp, na.rm = T)) %>%
-  ungroup()
+  ungroup() %>%
+  
+  select(-dl_025, -t) %>%
+  
+  left_join(
+    
+    paste0("https://www.procyclingstats.com/race/tour-de-france/", 2019, "/stage-1/results") %>%
+      xml2::read_html() %>% 
+      rvest::html_nodes('div.resultCont ') %>%
+      rvest::html_nodes('table') %>%
+      rvest::html_table() %>%
+      .[[1]] %>% 
+      select(bib = BIB, rider = Rider, team = Team) %>% 
+      unique() %>%
+      mutate(rider = str_sub(rider, 1, nchar(rider)-nchar(team))), by = c("bib")
+    
+  )
 
+#
+
+dist_vs_time <- expand_grid(dist = seq(0.1,10,0.1), speed = seq(10,50,1)) %>% mutate(tm_needed = (dist / speed)*3600)
+ 
+#
+#
+# ANALYSIS PARAMETERS
+
+start_d <- 50.25
+end_d <- 37.75
+
+#
+
+yellow_grp <- all_data %>% 
+  filter(distance_left >= 37.5 & distance_left <= 51.5 & in_yellow_grp == 1) %>% 
+  group_by(bib, rider) %>% 
+  count(sort = TRUE)
+
+#
+#
+#
+
+choose_riders <- c(61, 31, 2, 21)
+choose_riders <- c(2, 21, 81, 61, 101)
+choose_riders <- c(21, 108, 72, 15, 115, 107)
+
+choose_riders <- c(53, 51, 2, 21, 191)
+
+choose_riders <- c(51, 211, 65)
+
+choose_riders <- c(6,8,3,1,2)
+
+choose_riders <- c(2, 21, 65, 108, 1)
+
+#
+
+ggplot(all_data %>% 
+         filter(distance_left >= end_d & distance_left <= start_d & bib %in% choose_riders),
+       aes(x = distance_left, 
+           y = rolling_speed, 
+           color = as.factor(rider)))+
+  geom_point()+
+  geom_smooth(se = F, span = 0.35)+
+  scale_x_reverse()+
+  labs(x = "KMs left (Galibier summit is 19km, Lautaret summit is 29km)", 
+       y = "Speed rolling average in KM/H", 
+       title = "Quintana vs breakaway on Galibier (2019 TDF St18)")+
+  scale_color_discrete(name = "")
+  
+#
+#
 #
 
 r <- c(131, 21, 1, 2, 61, 65, 81, 51, 31, 12, 71, 25, 6, 8)
 
-ggplot(all_data %>% filter(distance_left > 0 & distance_left < 11 & bib %in% r & block < 402), 
+ggplot(all_data %>% filter(distance_left > 0 & distance_left < 8 & bib %in% r), 
        aes(x = distance_left, y = in_yellow_grp, color = as.factor(bib)))+
   
   geom_point()+
