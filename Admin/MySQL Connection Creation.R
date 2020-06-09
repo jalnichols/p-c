3268b33e8a56896cb4bf4984e293d1c97896e07a
jalnichols <nichols.jacob@gmail.com>
2020-06-09 02:04:36 +0000

    Set up Admin folder w/ move to RStudio Server

---
+++ Admin/MySQL Connection Creation.R
@@ -0,0 +1,16 @@
+
+# set up cycling database and connection
+
+# this will connect to DB instance w/o specifying database
+
+con <- DBI::dbConnect(
+  RMariaDB::MariaDB(), 
+  user = Sys.getenv("MYSQL_U"), 
+  password = Sys.getenv("MYSQL_P"), 
+  db = "", 
+  host = "p-c-database.cqrwmwxsn1fv.us-east-2.rds.amazonaws.com")
+
+# now create the database cycling
+
+dbSendQuery(con, "CREATE DATABASE IF NOT EXISTS cycling")
+
