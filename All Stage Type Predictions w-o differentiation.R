4c4f0b58e8a80058bbee46225312f4b116703caf
Jake Nichols <nichols.jacob@gmail.com>
2020-07-30 20:17:19 -0400

    Replace fatty glm model with slimmed down version

--- All Stage Type Predictions w-o differentiation.R
+++ All Stage Type Predictions w-o differentiation.R
@@ -3,6 +3,33 @@
 library(tidyverse)
 library(RMySQL)
 
+# define function to clean extraneous fat from GLM
+
+strip_glm = function(cm) {
+  cm$y = c()
+  cm$model = c()
+  
+  cm$residuals = c()
+  cm$fitted.values = c()
+  cm$effects = c()
+  cm$qr$qr = c()  
+  cm$linear.predictors = c()
+  cm$weights = c()
+  cm$prior.weights = c()
+  cm$data = c()
+  
+  
+  cm$family$variance = c()
+  cm$family$dev.resids = c()
+  cm$family$aic = c()
+  cm$family$validmu = c()
+  cm$family$simulate = c()
+  attr(cm$terms,".Environment") = c()
+  attr(cm$formula,".Environment") = c()
+  
+  cm
+}
+
 #
 
 con <- dbConnect(MySQL(),
@@ -807,7 +834,7 @@ summary(reg_mod)
 
 # write model // this writes an un-standardized/normalized version
 
-write_rds(reg_mod, "Stored models/basic-winprob-glm.rds")
+write_rds(strip_glm(reg_mod), "Stored models/basic-winprob-glm.rds")
 
 # predict test set out of sample
 pred <- cbind(coef = predict(mod, test_W),
