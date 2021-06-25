1b6716e988e084d908b7aeba4cdf940d0db028c0
Jake Nichols <nichols.jacob@gmail.com>
2021-06-24 21:25:34 -0400

    Add future looking with PCD/BS models + ability to bring in mature win prob model

--- Predictions from Model & Startlist.R
+++ Predictions from Model & Startlist.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(rvest)
 library(DBI)
 
+DBI::dbDisconnect(con)
+
 con <- dbConnect(RMySQL::MySQL(),
                  host='localhost',
                  dbname='cycling',
@@ -11,10 +13,35 @@ con <- dbConnect(RMySQL::MySQL(),
 
 #
 
-URL <- 'https://www.procyclingstats.com/race/giro-d-italia/2021/startlist/'
+URL <- 'https://www.procyclingstats.com/race/tour-de-france/2021/startlist/'
 
-PCD = 18
-BS = 0.001
+TVG <- 2800 #2800
+KM <- 198 #198
+UphillFinish = 1
+ST <- "icon profile p3"
+STAGE = 1
+PV = 76 #75
+
+PCD1 <- predict(read_rds("Stored models/pcd-pv-mod.rds"), tibble(pv = PV))
+PCD2 <- predict(read_rds("Stored models/pcd-icon-mod.rds"), tibble(stage_type = ST, class_level = 4))
+PCD3 <- predict(read_rds("Stored models/strava-elev-mod.rds"), tibble(tvg = TVG / KM, strava_elevation = TVG, stage_type = ST))
+
+PCD = (PCD1 + PCD2 + PCD3) / 3
+
+BS <- predict(read_rds("Stored models/bunchsprint-glm-mod.rds"), 
+              tibble(pred_climb_difficulty = PCD,
+                     finalGT = 0,
+                     grand_tour = 1,
+                     length = KM - 200,
+                     cobbles = 0,
+                     one_day_race = 0,
+                     perc_thru = STAGE / 21,
+                     uphill_finish = UphillFinish,
+                     sq_pcd = PCD ^ 2
+                     
+                     ))
+
+BS <- exp(BS)/(1+exp(BS))
 
 #
 
@@ -102,6 +129,17 @@ most_recent_models <- rbind(
              WHERE test_or_prod = 'prod'") %>% mutate(Type = 'Points'),
   
   dbGetQuery(con, "SELECT rider, random_intercept, pcd_impact, r.Date, bunchsprint_impact
+             FROM lme4_rider_wins r
+             JOIN (
+             
+             SELECT max(Date) as Date
+             FROM lme4_rider_wins r
+             WHERE test_or_prod = 'prod'
+             
+             ) x ON r.Date = x.Date
+             WHERE test_or_prod = 'prod'") %>% mutate(Type = 'Wins'),
+  
+  dbGetQuery(con, "SELECT rider, random_intercept, pcd_impact, r.Date, bunchsprint_impact
              FROM lme4_rider_teamleader r
              JOIN (
              
@@ -125,27 +163,30 @@ models <- riders %>%
     most_recent_models, by = c("rider")
     
   ) %>%
-
+  
   mutate(coef = ifelse(Type == "Points", random_intercept + (pcd_impact * PCD) + (bunchsprint_impact * BS) + 0.012,
                        ifelse(Type == "PointsWhenOpp", random_intercept + (pcd_impact * PCD) + (bunchsprint_impact * BS) + 0.012,
-                       ifelse(Type == "LogRanks",  random_intercept + (pcd_impact * PCD) + (bunchsprint_impact * BS),
-                              ifelse(Type == "TimeLost", random_intercept + (pcd_impact * PCD) + (bunchsprint_impact * BS),
-                                     ifelse(Type == "Leader", exp(random_intercept + (pcd_impact * PCD) + (bunchsprint_impact * BS) - 2), 
-                                            ifelse(Type == "WhenOpp", exp(random_intercept + (pcd_impact * PCD) + (bunchsprint_impact * BS) - 2), NA)))))),
+                              ifelse(Type == "LogRanks",  random_intercept + (pcd_impact * PCD) + (bunchsprint_impact * BS),
+                                     ifelse(Type == "TimeLost", random_intercept + (pcd_impact * PCD) + (bunchsprint_impact * BS),
+                                            ifelse(Type == "Leader", exp(random_intercept + (pcd_impact * PCD) + (bunchsprint_impact * BS) - 2),
+                                                   ifelse(Type == "Wins", exp(random_intercept + (pcd_impact * PCD) + (bunchsprint_impact * BS) - 6.5), 
+                                                          ifelse(Type == "WhenOpp", exp(random_intercept + (pcd_impact * PCD) + (bunchsprint_impact * BS) - 2), NA))))))),
          coef = ifelse(Type %in% c("Success", "Leader", "WhenOpp"), coef / (1+coef), coef)) %>%
   
   mutate(coef = ifelse(is.na(coef),
                        ifelse(Type %in% c("Points", "PointsWhenOpp"), 0.01,
-                              ifelse(Type == "LogRanks", min(coef, na.rm = T),
+                              ifelse(Type %in% c("LogRanks", "Wins"), min(coef, na.rm = T),
                                      ifelse(Type == "Leader", 0.02,
                                             ifelse(Type %in% c("Success","WhenOpp"), 0.01,
                                                    ifelse(Type == "TimeLost", max(coef, na.rm = T), NA))))), coef)) %>%
   group_by(team, Type) %>%
-  mutate(coef = ifelse(Type == "Leader", ifelse(coef <= 0.05, 0, coef), coef)) %>%
+  mutate(coef = ifelse(Type == "Leader", ifelse(coef <= 0.1, 0, coef), coef)) %>%
   ungroup() %>%
   
+  mutate(adj = ifelse(Type == "Wins", coef / sum(coef), coef)) %>%
+  
   group_by(team, Type) %>%
-  mutate(adj = ifelse(Type == "Leader", coef / sum(coef), coef)) %>%
+  mutate(adj = ifelse(Type %in% c("Leader"), coef / sum(coef), coef)) %>%
   ungroup()
 
 #
@@ -157,12 +198,13 @@ wide_models <- models %>%
   select(rider, team, Type, adj) %>%
   spread(Type, adj) %>%
 
-  mutate(rankPoints = rank(-Points, ties.method = "min"),
-         rankPointsWhenOpp = rank(-PointsWhenOpp, ties.method = "min"),
-         rankLog = rank(-LogRanks, ties.method = "min"),
-         rankTimeLost = rank(TimeLost, ties.method = "min")) %>%
+  mutate(rankPoints = rank(desc(Points), ties.method = "min"),
+         rankPointsWhenOpp = rank(desc(PointsWhenOpp), ties.method = "min"),
+         rankLog = rank(desc(LogRanks), ties.method = "min"),
+         rankTimeLost = rank(TimeLost, ties.method = "min"),
+         rankWins = rank(desc(Wins), ties.method = "min")) %>%
 
-  mutate(rankOverall = 4 / ((1/rankPoints)+(1/rankLog)+(1/rankPointsWhenOpp)+(1/rankTimeLost)))
+  mutate(rankOverall = 4 / ((1/rankPoints)+(1/rankLog)+(1/rankPointsWhenOpp)+(1/rankWins)))
 
 #
 
@@ -274,3 +316,100 @@ team_ranks <- all_models %>%
   mutate(stat = ifelse(str_detect(type, "coef"), round(top5 + mean,0)/2, harm)) %>%
   select(type, stat, team) %>%
   spread(type, stat)
+
+#
+#
+#
+#
+#
+#
+#
+
+recent_perf <- dbGetQuery(con, "SELECT rider, date, (act_leader - exp_leader) as rel_leader, 
+    exp_leader, act_leader, exp_rank, act_rank, error_rank, races, days_of_comps
+               FROM performance_last10races_vsmodel") %>%
+  mutate(date = as.Date(date)) %>%
+  filter(date == max(date))
+
+win_prob_model <- read_rds("Stored models/final_win_prob_model.rds")
+breakaway_model <- read_rds("Stored models/breakaway_glm.rds")
+
+BREAKAWAY_PRED <- predict(breakaway_model, 
+                          tibble(pred_climb_difficulty = PCD, predicted_bs = BS, uphill_finish = UphillFinish, grand_tour = 1))
+
+BREAKAWAY_PRED = exp(BREAKAWAY_PRED) / (1+exp(BREAKAWAY_PRED))
+
+# overwrite
+BREAKAWAY_PRED <- 0.1
+
+#
+
+applying_next_level <- models %>%
+  
+  filter(Type %in% c("Leader", "PointsWhenOpp")) %>%
+  
+  select(rider, team, Type, adj) %>%
+  spread(Type, adj) %>%
+  
+  rename(pred_pointswhenopp = PointsWhenOpp,
+         shrunk_teamldr = Leader) %>%
+  mutate(rel_age = 0) %>%
+  
+  group_by(team) %>%
+  mutate(No1_Team = rank(desc(shrunk_teamldr), ties.method = "min") == 1,
+         No1_Team = as.numeric(No1_Team)) %>%
+  ungroup() %>%
+  
+  mutate(rk_pointswhenopp = rank(desc(pred_pointswhenopp), ties.method = 'min')) %>%
+  
+  left_join(
+    
+    recent_perf, by = c('rider')
+    
+  ) %>%
+  
+  mutate(error_rank = ifelse(is.na(error_rank), 0, error_rank),
+         rel_leader = ifelse(is.na(rel_leader), 0, rel_leader),
+         races = ifelse(is.na(races), 0, races),
+         days_of_comps = ifelse(is.na(days_of_comps), 0, days_of_comps)) %>%
+  
+  # center error rank and adjust for <10 races
+  mutate(error_rank = error_rank / (1 / races) * 0.1) %>%
+  
+  mutate(error_rank = error_rank - mean(error_rank, na.rm = T)) %>%
+  
+  mutate(log_rk_pwo = log(rk_pointswhenopp + 1),
+         ppwo = pred_pointswhenopp * -1) %>%
+  
+  mutate(pred_break_win = BREAKAWAY_PRED) %>%
+  
+  mutate(rider_match = str_to_title(rider)) %>%
+  
+  left_join(
+    
+    dbGetQuery(con, "SELECT rider, date as dob FROM rider_attributes") %>%
+      
+      mutate(rider = str_to_title(rider)), by = c("rider_match" = "rider")) %>%
+  
+  mutate(age = as.numeric(as.Date(date)-as.Date(dob))/365.25) %>%
+
+  mutate(rel_age = age - mean(age, na.rm = T),
+         rel_age = ifelse(is.na(rel_age), 0, rel_age)) %>%
+  
+  select(-dob, -age, -rider_match)
+
+#
+
+win_probabilities <- cbind(
+  
+  win_probability = predict(win_prob_model, applying_next_level),
+  
+  applying_next_level
+  
+) %>%
+  
+  mutate(win_probability = exp(win_probability) / (1 + exp(win_probability)),
+         win_probability = win_probability / sum(win_probability)) %>%
+  
+  select(-days_of_comps, -races, -exp_leader, -act_leader,
+         -exp_rank, -act_rank, -ppwo, -pred_break_win)
