36244808e0c974d9a6c9836d7f592a8a1e0afa48
Jake Nichols <nichols.jacob@gmail.com>
2022-03-21 19:47:39 -0400

    Improve how access date and bet type are stored

--- Load Unibet Odds.R
+++ Load Unibet Odds.R
@@ -13,11 +13,16 @@ con <- dbConnect(MySQL(),
 
 #
 
-betway <- fs::dir_info("C:/Users/Jake/Documents/R Code/p-c/odds/unibet-odds/")
+betway <- fs::dir_info("C:/Users/Jake/Documents/R Code/p-c/odds/unibet-odds/") %>%
+  
+  mutate(file_path = str_replace(path, "C:/Users/Jake/Documents/R Code/p-c/odds/unibet-odds/", ""),
+         accessed_at = str_replace_all(str_sub(file_path, 12,30), "_", ":"),
+         accessed_at = lubridate::as_datetime(accessed_at),
+         eventId = str_sub(file_path, 1, 10))
 
 #
 
-dbSendQuery(con, "DELETE FROM unibet_cycling_odds")
+#dbSendQuery(con, "DELETE FROM unibet_cycling_odds")
 
 #
 
@@ -30,7 +35,7 @@ for(f in 1:length(betway$path)) {
   event_data <- tibble(unibetId = page$events$id,
                        name = page$events$name,
                        start_time = page$events$start, 
-                       accessed_at = betway$birth_time[[f]][[1]])
+                       accessed_at = betway$accessed_at[[f]][[1]])
   
   #
   
@@ -53,11 +58,18 @@ for(f in 1:length(betway$path)) {
                  unibetId = page$betOffers$eventId[[x]]) %>%
           
           cbind(offers[x,] %>% 
+                  select(criterion) %>% 
+                  unnest(cols = c("criterion")) %>%
+                  select(betSpecType = label)) %>%
+          
+          cbind(offers[x,] %>% 
                   select(betOfferType, marketId = id) %>% 
                   unnest(cols = c("betOfferType")) %>%
                   select(betType = name, marketId)) %>%
           
-          inner_join(event_data, by = c("unibetId"))
+          inner_join(event_data, by = c("unibetId")) %>%
+          
+          mutate(file_path = str_replace(betway$path[[f]], "C:/Users/Jake/Documents/R Code/p-c/odds/unibet-odds/", ""))
         
         #
         
@@ -85,4 +97,53 @@ all_odds <- dbReadTable(con, "unibet_cycling_odds") %>%
 #
 #
 
+all_odds %>% 
+  filter(name == 'Milano - Sanremo 2022' & betType == "Winner" & betSpecType == "Winner") %>%
+  
+  filter(!is.na(rider)) %>%
+  
+  group_by(accessed_at) %>% 
+  mutate(implied = 1/odds,
+         implied = implied / sum(implied, na.rm = T)) %>% 
+  ungroup() %>% 
+  
+  filter(rider %in% c("Van Aert Wout", "Asgreen Kasper", "Van Der Poel Mathieu", "Ganna Filippo",
+                      "Pedersen Mads", "Pidcock Thomas", "Alaphilippe Julian",
+                      "Ewan Caleb", "Pogacar Tadej", "Mohoric Matej")) %>%
+  
+  group_by(rider, 
+           date = as.Date(lubridate::as_datetime(accessed_at))) %>% 
+  summarize(odds = mean(implied)) %>% 
+  ungroup() %>% 
+  
+  ggplot(aes(x = date, y = odds, color = rider))+
+  
+  geom_point(size=4, alpha = 0.5)+
+  geom_line()+
+  scale_y_continuous(labels = scales::percent)+labs(x = "", y = "implied probability", title = "Paris Roubaix Odds")
 
+#
+#
+#
+
+all_odds %>% 
+  filter(name == 'General Classification (Tour de France 2022)') %>%
+  
+  group_by(accessed_at) %>% 
+  mutate(implied = 1/odds,
+         implied = implied / sum(implied, na.rm = T)) %>% 
+  ungroup() %>% 
+  
+  filter(rider %in% c("Pogacar Tadej", "Roglic Primoz", "Thomas Geraint", "Quintana Nairo", 'Pinot Thibaut', "Mas Enric", "Martinez Daniel Felipe", "Carapaz Richard", "Vingegaard Jonas", "Haig Jack")) %>%
+  
+  group_by(rider, date = as.Date(lubridate::as_datetime(accessed_at))) %>% 
+  summarize(odds = mean(implied)) %>% 
+  ungroup() %>% 
+  
+  ggplot(aes(x = date, y = odds, color = rider))+
+  
+  geom_vline(xintercept = c(as.Date('2022-03-05'), as.Date('2022-03-12'), as.Date('2022-03-19')))+
+  
+  geom_point(size=4, alpha = 0.5)+
+  geom_line()+
+  scale_y_continuous(labels = scales::percent)+labs(x = "", y = "implied probability", title = "Paris Roubaix Odds")
